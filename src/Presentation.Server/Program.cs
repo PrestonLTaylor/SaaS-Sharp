9f4ea993b98aac8a75479e9865d228d0dfc44bec
Preston Taylor <95388976+PrestonLTaylor@users.noreply.github.com>
2024-04-12 14:02:02 +0100

    chore: Add the client and server blazor projects

---
+++ src/Presentation.Server/Program.cs
@@ -0,0 +1,31 @@
+using Presentation.Server.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Presentation.Client._Imports).Assembly);
+
+app.Run();
